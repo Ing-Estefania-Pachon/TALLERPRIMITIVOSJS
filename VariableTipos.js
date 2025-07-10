4467b7121cdfdff873efc0e1d86ff18071538ab8
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-10 16:18:52 -0500

    Cambie los nombre de los archivos

---
+++ VariableTipos.js
@@ -0,0 +1,17 @@
+// ! Ejercicio 1:
+
+//  •Crea una variable nombre con tu nombre.
+let nombre = "Estefania Pachon"
+
+//  •Crea una variable edad con tu edad.
+let edad = 23
+
+//  •Crea una variable esEstudiante con true o false.
+let esEstudiante = true
+
+// !  Ejercicio 2:
+//  •Declara una variable sinValor sin asignarle nada (queda undefined).
+
+//  •Declara una variable valorNulo y asígnale null.
+
+//  •Muestra ambas en consola y observa la diferencia.
