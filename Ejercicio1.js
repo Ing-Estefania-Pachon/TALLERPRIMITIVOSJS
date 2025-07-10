3829bf1703b1e78a05188f0f4501358fcdba97a8
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-10 16:13:45 -0500

    Agregue la solución al ejercicio 1 en el archivo Ejercicio1

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+// ! Ejercicio 1:
+
+//  •Crea una variable nombre con tu nombre.
+let nombre = "Estefania Pachon"
+
+//  •Crea una variable edad con tu edad.
+let edad = 23
+
+//  •Crea una variable esEstudiante con true o false.
+let esEstudiante = true
